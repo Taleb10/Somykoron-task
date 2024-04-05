c2bb4c276fa90f1bfd19a88392b42bd9f73789a8
Taleb Ibne Hossain Khan <taleblog3@gmail.com>
2024-04-06 01:36:35 +0600

    First Commit

---
+++ somykoron-task/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavBarTailwind from "./components/NavBarTailwind";
+import Sform from "./components/Sform";
+
+function App() {
+  return (
+    <>
+      <NavBarTailwind />
+      <div className="">
+        <Sform />
+      </div>
+    </>
+  );
+}
+
+export default App;
